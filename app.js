670bbedc7c3ba50a1299b9a22d44496cc33473fd
Devin0076 <ussery.devin@ic.edu>
2025-12-03 19:34:05 -0600

    MVP commit 8: Added Express app and server setup

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const sequelize = require('./config/database');
+const initModels = require('./models');
+const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+
+// Create Express app
+const app = express();
+const models = initModels(sequelize);
+
+// Middleware
+app.use(express.json());
+app.use(logger);
+
+// Attach models to request object
+app.use((req, res, next) => {
+  req.models = models;
+  next();
+});
+
+// Root route
+app.get('/', (req, res) => {
+  res.json({ message: "Pit / Dark Pit Matchup API MVP" });
+});
+
+// Routes
+app.use('/characters', require('./routes/characters'));
+// Other routes will be added later:
+// app.use('/ban-stages', ...)
+// app.use('/counterpick-stages', ...)
+// app.use('/tips', ...)
+
+app.use(errorHandler);
+
+module.exports = app;
