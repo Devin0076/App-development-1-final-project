31cc393fece334619fb9fbd762cc1ce62e05c812
Devin0076 <ussery.devin@ic.edu>
2025-12-10 17:02:18 -0600

    Final Commit 4: Has user passwords with bcrypt in User model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,9 @@
 // User model for authentication and role-based access
 
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 10;
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     id: {
@@ -17,14 +21,36 @@ module.exports = (sequelize, DataTypes) => {
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false, 
+      allowNull: false, // stores the hashed password
     },
     role: {
       type: DataTypes.STRING,
       allowNull: false,
-      defaultValue: 'user', 
+      defaultValue: 'user', // default role is regular user
     },
+  }, {
+    hooks: {
+      // Hash password before creating a new user
+      beforeCreate: async (user) => {
+        if (user.password) {
+          const hashed = await bcrypt.hash(user.password, SALT_ROUNDS);
+          user.password = hashed;
+        }
+      },
+      // Hash password again if it is changed on update
+      beforeUpdate: async (user) => {
+        if (user.changed('password')) {
+          const hashed = await bcrypt.hash(user.password, SALT_ROUNDS);
+          user.password = hashed;
+        }
+      }
+    }
   });
+
+  // Instance method to check a plain text password against the stored hash
+  User.prototype.checkPassword = async function (plainPassword) {
+    return bcrypt.compare(plainPassword, this.password);
+  };
 
   return User;
 };
